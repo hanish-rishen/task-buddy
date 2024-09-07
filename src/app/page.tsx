4c677dc4b6efb3ed4268868e18a6392d3ed49597
Hanish Rishen <hanishrishen@gmail.com>
2024-09-07 19:12:09 +0530

    Repeat delay removed from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,6 @@ export default function Home() {
         numSquares={30}
         maxOpacity={0.1}
         duration={3}
-        repeatDelay={1}
         className="[mask-image:radial-gradient(500px_circle_at_center,white,transparent)] inset-x-0 inset-y-[-30%] h-[200%] skew-y-12"
       />
       
